99a99565a042a06a0537d8561c8a7870309c52ac
郭立平 <liping0_0@my.swjtu.edu.cn>
2021-10-10 23:29:20 +0800

    updated

--- signal_convolution.py
+++ signal_convolution.py
@@ -34,7 +34,7 @@ if __name__ == '__main__':
 
 	n_fit = int(0.1*500)	# 第一个常数，表示用于待处理数据中可用于抛物线拟合的捕捉数据点数量
 	n_add = int(0.1*500)	# 第二个常数，表示在信号首尾端需要添加的数据点数量
-	scale =6  # 小波函数尺度参数 T=0.094s, fs=500Hz，伪中心频率0.12699对应的尺度参数为5.96853
+	scale =128  # 小波函数尺度参数 T=0.094s, fs=500Hz，伪中心频率0.12699对应的尺度参数为5.96853
 	key_i = int((len(time_updated)-2*n_add-1)*0.93)  # 关键索引，便于求解小波变换幅值参数0.918for12
 
 	time_updated, disp_updated = func_user_pad(time_updated, disp_updated, n_fit, 'before', n_add)
@@ -207,12 +207,24 @@ if __name__ == '__main__':
 	cccc1 = Amp1_test_utn*test_utn_conv1
 	cccc2 = Amp2_test_utn*test_utn_conv2
 
+
+	plt.plot(analy_t,analy_ut)
+	plt.plot(analy_t,analy_ut)
+	plt.plot(analy_t,analy_vt)
+	plt.plot(analy_t,analy_at)
+
 	np.savetxt('analy_t.txt', analy_t)
-	np.savetxt('analy_utn.txt', analy_utn[n_add:-n_add])	
+	np.savetxt('analy_ut.txt', analy_ut)
+	np.savetxt('analy_vt.txt', analy_vt)
+	np.savetxt('analy_at.txt', analy_at)
+	#np.savetxt('analy_utn.txt', analy_utn[n_add:-n_add])	
+	#np.savetxt('analy_vtn.txt', analy_vtn[n_add:-n_add])	
+	#np.savetxt('analy_atn.txt', analy_atn[n_add:-n_add])	
+
 	np.savetxt('test_time.txt', test_time)
-	np.savetxt('Amp0Myconv0.txt', cccc0)
-	np.savetxt('Amp1Myconv1.txt', cccc1)
-	np.savetxt('Amp2Myconv2.txt', cccc2)
+	np.savetxt('Amp0Myconv0.txt', Amp0_analy_utn*analy_utn_conv0)
+	np.savetxt('Amp1Myconv1.txt', Amp1_analy_utn*analy_utn_conv1)
+	np.savetxt('Amp2Myconv2.txt', Amp2_analy_utn*analy_utn_conv2)
 
 	'''
 	# 绘制卷积运算、pywt计算结果
